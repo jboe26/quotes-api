7a5df0af6e27462f61750d233aa6716d132666a8
Josh Boepple <joshrboepple@outlook.com>
2025-03-19 20:40:42 -0400

    updated

---
+++ api/index.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+
+if (!empty($request_uri[1])) {
+    switch ($request_uri[1]) {
+        case 'quotes':
+            include_once 'quotes/index.php';
+            break;
+        case 'authors':
+            include_once 'authors/index.php';
+            break;
+        case 'categories':
+            include_once 'categories/index.php';
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode(["message" => "Endpoint not found"]);
+            break;
+    }
+} else {
+    // Redirect to index.html instead of showing JSON API info
+    header("Location: /index.html");
+    exit();
+}
+?>
